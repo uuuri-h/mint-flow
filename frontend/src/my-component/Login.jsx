1b3b67dd77e456f2e7fb526a8ac2c255a2ffd09d
uuuri-h <lilou.00q@gmail.com>
2026-05-04 13:45:30 +0900

    20260504:フロント途中

---
+++ frontend/src/my-component/Login.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function MyComponent() {
+    const [data, setData] = useState('');
+
+    useEffect(() => {
+    axios.get('http://localhost:8000/api/data')
+        .then(response => {
+        setData(response.data.message);
+    })
+        .catch(error => {
+        console.error('There was an error!', error);
+    });
+}, []);
+
+    return (
+        <div>
+            <h1>Data from FastAPI:</h1>
+            <p>{data}</p>
+        </div>
+    );
+}
+
+export default MyComponent;
