ca13972d809c0149e6f8e30324878126d0377238
uuuri-h <lilou.00q@gmail.com>
2026-05-30 22:14:26 +0900

    2026530:依頼件数表示

--- frontend/src/my-pages/OrderList/OrderTable.jsx
+++ frontend/src/my-pages/OrderList/OrderTable.jsx
@@ -12,6 +12,10 @@ function OrderTable() {
         2: '発注済み',
         99: 'キャンセル'
     };
+
+    const [page, setPage] = useState(1);
+    const [totalItems, setTotalItems] = useState(0);
+
     {/* const[状態, 状態を更新する関数] = useState(初期値) */ }
 
     useEffect(() => {
@@ -24,6 +28,8 @@ function OrderTable() {
                 console.log('response.data.requests:', response.data.requests)
 
                 setOrders(response.data.requests)
+                setTotalItems(response.data.requests.length) 
+
             } catch (error) {
                 console.error('発注依頼データの取得に失敗しました:', error);
             }
@@ -33,70 +39,58 @@ function OrderTable() {
     }, []);
 
     return (
-        <div className="order-table-container">
-            <table className="order-table">
-                {/* <colgroup>
-                    <col className="col1" style={{ width: '8%' }} />
-                    <col className="col2" style={{ width: '15%' }} />
-                    <col className="col3" style={{ width: '15%' }} />
-                    <col className="col4" style={{ width: '15%' }} />
-                    <col className="col5" style={{ width: '15%' }} />
-                    <col className="col6" style={{ width: '15%' }} />
-                    <col className="col7" style={{ width: '15%' }} />
-                    <col className="col8" style={{ width: '15%' }} />
-                    <col className="col10" style={{ width: '8%' }} />
-                </colgroup> */}
-                <thead>
-                    <tr 
-                    >
+        <div className="order-table-wrapper"> 
+            <p className='total-items'>件数（ <span className='total-items-count'>{totalItems}</span> 件）</p>
+            <div className="order-table-container">
+                <table className="order-table">
+                    {/* <colgroup>
+                        <col className="col1" style={{ width: '8%' }} />
+                        <col className="col2" style={{ width: '15%' }} />
+                        <col className="col3" style={{ width: '15%' }} />
+                        <col className="col4" style={{ width: '15%' }} />
+                        <col className="col5" style={{ width: '15%' }} />
+                        <col className="col6" style={{ width: '15%' }} />
+                        <col className="col7" style={{ width: '15%' }} />
+                        <col className="col8" style={{ width: '15%' }} />
+                        <col className="col10" style={{ width: '8%' }} />
+                    </colgroup> */}
+                    <thead>
+                        <tr 
+                        >
 
-                        <th className="th1"></th>
-                        <th className='th2'>依頼日</th>
-                        <th className='th3'>依頼ID</th>
-                        <th className='th4'>顧客名</th>
-                        <th className='th5'>アイテム数</th>
-                        <th className='th6'>合計金額</th>
-                        <th className='th7'>納期</th>
-                        <th className='th9'>依頼ステータス</th>
-                        <th className='th10'></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* ここに発注データをマッピングして表示 */}
-                    {orders.map((order) => (
-                        <tr key={order.id}>
-                            <td className="td1"><input type="checkbox" className="check-box" /></td>
-                            <td className="td2">{order.request_date}</td>
-                            <td className="td3">{order.request_id}</td>
-                            <td className="td4">{order.customer_name}</td>
-                            <td className="td5">{order.item_count}</td>
-                            <td className="td6">￥{order.total_amount}</td>
-                            <td className="td7">{order.delivery_date}</td>
-                            <td className="td9">{statusMap[order.status]}</td>
-                            <td className="td10">
-                                <button className="btn btn-primary">編集</button>
-                            </td>
+                            <th className="th1"></th>
+                            <th className='th2'>依頼日</th>
+                            <th className='th3'>依頼ID</th>
+                            <th className='th4'>顧客名</th>
+                            <th className='th5'>アイテム数</th>
+                            <th className='th6'>合計金額</th>
+                            <th className='th7'>納期</th>
+                            <th className='th9'>依頼ステータス</th>
+                            <th className='th10'></th>
                         </tr>
-                    ))}
-                    <tr>
-                        <td className="td1"><input type="checkbox" className="check-box" /></td>  
-                        <td className="td2">2023-10-01</td>
-                        <td className="td3">JOB26-00022</td>
-                        <td className="td4">A COMPANY</td>
-                        <td className="td5">10</td>
-                        <td className="td6">￥1797300</td>
-                        <td className="td7">2023-10-01</td>
-                        <td className="td9">処理中</td>
-                        <td className="td10">
-                            <button className="btn btn-primary">編集</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
+                    </thead>
+                    <tbody>
+                        {/* ここに発注データをマッピングして表示 */}
+                        {orders.map((order) => (
+                            <tr key={order.id}>
+                                <td className="td1"><input type="checkbox" className="check-box" /></td>
+                                <td className="td2">{order.request_date}</td>
+                                <td className="td3">{order.request_id}</td>
+                                <td className="td4">{order.customer_name}</td>
+                                <td className="td5">{order.item_count}</td>
+                                <td className="td6">￥{order.total_amount}</td>
+                                <td className="td7">{order.delivery_date}</td>
+                                <td className="td9">{statusMap[order.status]}</td>
+                                <td className="td10">
+                                    <button className="btn btn-primary">編集</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-
-    )
+    );
 }
 
-export default OrderTable
+export default OrderTable;
